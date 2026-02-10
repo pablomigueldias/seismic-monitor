ea44fa458dd8cca1957bc3538d2631f324bac8dd
pablomigueldias <pablo.miguel.dias@gmail.com>
2026-02-09 23:57:06 -0300

    update1

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const baseURL = import.meta.env.VITE_API_URL || 'http://localhost:8000';
+
 export const api = axios.create({
-  baseURL: 'http://localhost:8000/api/v1', 
+  baseURL: `${baseURL}/api/v1`, 
 });
 
 
