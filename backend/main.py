dcf463d9d933a267da6397baf8fdf4702d8bd9c6
pablomigueldias <pablo.miguel.dias@gmail.com>
2026-02-10 00:13:36 -0300

    update4

--- backend/main.py
+++ backend/main.py
@@ -16,7 +16,7 @@ origins = [
     "http://localhost",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://seismic-monitor.vercel.app/"
+    "https://seismic-monitor.vercel.app/",
     "https://seismic-monitor.vercel.app/api/v1/earthquakes"
 ]
 
